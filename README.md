60390b55bc957317265b8603f2fdbc37038c0b29
Tomislav <tgjuranec@gmail.com>
2026-01-09 20:21:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# csv_to_graph
+
+Simple program to convert data in csv format to graph in png format.
+
+
---
+++ analyze.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from array import array
+from matplotlib.dates import DateFormatter
+
+# Read CSV with proper datetime parsing
+df = pd.read_csv('ha.csv', parse_dates=[2],na_values=['unavailable'])
+df.columns = ['device','temp', 'timestamp']
+
+df = df.dropna()
+
+fig, ax = plt.subplots(figsize=(12, 6))
+ax.plot(df['timestamp'], df['temp'],linewidth=1.5)
+# Format x-axis as DD-HH-MM
+date_format = DateFormatter('%d-%H-%M')
+ax.xaxis.set_major_formatter(date_format)
+
+
+# Create plot
+plt.figure(figsize=(12, 6))
+plt.plot(df['timestamp'], df['temp'], linewidth=1.5)
+plt.xlabel('Time')
+plt.ylabel('Temperature')
+plt.title('Plumbing Antifreezing Project')
+plt.grid(True,which='major',alpha=0.3)
+plt.tight_layout()
+
+
+ymin,ymax = plt.ylim()
+y_ticks = [float(x/10) for x in range(int(ymin*10), int((ymax+1)*10), 5)]
+print(ymin,ymax)
+plt.yticks(y_ticks)
+plt.savefig('output_plot.png', dpi=300)
+plt.show()
+
+
