73bf5c98d7200c10580de3439efc7ded46191ba3
Tomislav <tgjuranec@gmail.com>
2026-01-09 21:05:05 +0100

    Time presentation solved, .gitignore added

--- analyze.py
+++ analyze.py
@@ -8,7 +8,7 @@ from matplotlib.dates import DateFormatter
 # Read CSV with proper datetime parsing
 df = pd.read_csv('ha.csv', parse_dates=[2],na_values=['unavailable'])
 df.columns = ['device','temp', 'timestamp']
-
+# Drop all invalid data
 df = df.dropna()
 
 fig, ax = plt.subplots(figsize=(12, 6))
@@ -19,20 +19,19 @@ ax.xaxis.set_major_formatter(date_format)
 
 
 # Create plot
-plt.figure(figsize=(12, 6))
-plt.plot(df['timestamp'], df['temp'], linewidth=1.5)
-plt.xlabel('Time')
-plt.ylabel('Temperature')
-plt.title('Plumbing Antifreezing Project')
+
+
 plt.grid(True,which='major',alpha=0.3)
 plt.tight_layout()
-
+ax.set_xlabel('Time')
+ax.set_ylabel('Value')
+ax.set_title('Temperature ')
+ax.grid(True, alpha=0.3)
 
 ymin,ymax = plt.ylim()
 y_ticks = [float(x/10) for x in range(int(ymin*10), int((ymax+1)*10), 5)]
 print(ymin,ymax)
 plt.yticks(y_ticks)
 plt.savefig('output_plot.png', dpi=300)
-plt.show()
 
 
