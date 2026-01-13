e6528fcc6b40ae399514d9fa8fc2baa004d1edf8
Tomislav <tgjuranec@gmail.com>
2026-01-13 20:14:30 +0100

    Added 14th January

--- compare_meteo_data.sh
+++ compare_meteo_data.sh
@@ -2,8 +2,13 @@
 #
 #
 
-curl "https://archive-api.open-meteo.com/v1/era5?latitude=45.87&longitude=15.76&start_date=2021-01-01&end_date=2021-12-31&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
+DATE1="2026-01-04"
+DATE2="2026-01-13"
+
+source .venv/bin/activate
+
+curl "https://archive-api.open-meteo.com/v1/era5?latitude=45.87&longitude=15.76&start_date=$DATE1&end_date=$DATE2&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m" > meteo.json
 
 echo "time,temp,humidity,wind" > meteo.csv && jq -r '.hourly | ([.time, .temperature_2m, .relative_humidity_2m, .wind_speed_10m] | transpose[] | @csv)' meteo.json >> meteo.csv
 
-
+python3 analyze.py
